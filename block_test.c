9e409ce305554db560290a72a57f42f411e30927
root <root@localhost.localdomain>
2019-07-26 09:10:22 -0400

    Complete a simple block driver module, and add one device. Such block dirver just directly destroys all bio by calling bio_endio()

--- block_test.c
+++ block_test.c
@@ -12,10 +12,11 @@
 
 #define DRIVER_NAME "block test driver"
 #define DEVICE_NAME "bt_dev"
-#define DEVICE_MINOR 1
+#define DEVICE_MINOR 0
 
 //#define BLOCK_TEST_MAJOR 16
 #define SECTOR_BITS 9
+#define DEV_SIZE    (512UL<< 20) 
 
 
 struct block_test_dev {
@@ -44,9 +45,7 @@ static int do_block_test_request(struct request_queue *q, struct bio *bio)
 }
 
 static int block_test_open(struct block_device *bdev, fmode_t mode) {
-    int device;
-    device = MINOR(bdev->bd_dev);
-    printk("device is open by: block_test [%d]\n", device);
+    printk("device is open by: block_test\n");
     return 0;
 }
 
@@ -55,10 +54,10 @@ static int block_test_release(struct gendisk *disk, fmode_t mode) {
     return 0;
 }
 
-static const struct block_device_operations block_dev_fops = {
-    .owner = THIS_MODULE,
-    .open = block_test_open,
-    .release = block_test_release,
+static struct block_device_operations block_dev_fops = {
+    .owner        = THIS_MODULE,
+    .open         = block_test_open,
+    .release      = block_test_release,
 };
 
 static int __init block_test_init(void)
@@ -66,38 +65,50 @@ static int __init block_test_init(void)
     int ret;
 	ret = -EBUSY;
 
+    /*
 #ifdef BLOCK_TEST_MAJOR
 	if (register_blkdev(BLOCK_TEST_MAJOR, "block_test"))
 		goto out1;
     block_test_major = BLOCK_TEST_MAJOR;
 #else
+*/
     block_test_major = register_blkdev(block_test_major, "block_test");
-    if (block_test_major < 0)
+    if (block_test_major < 0) {
+        printk(KERN_CRIT "bldev register fail!\n");
         goto out1;
+    }
+    /*
 #endif
+*/
 
 	ret = -ENOMEM;
     block_test_gendisk = alloc_disk(1);
-    if (!block_test_gendisk)
+    if (!block_test_gendisk) {
+        printk(KERN_CRIT "alloc disk fail!\n");
         goto out2;
+    }
+    bt_dev = kmalloc(sizeof (struct block_test_dev), GFP_KERNEL);
+    bt_dev->disk = block_test_gendisk;
 
     block_test_queue = blk_alloc_queue(GFP_KERNEL);
-	if (!block_test_queue)
+	if (!block_test_queue) {
+        printk(KERN_CRIT "alloc queue fail!\n");
 		goto out_queue;
+    }
     blk_queue_make_request(block_test_queue, do_block_test_request);
 
-    bt_dev = kmalloc(sizeof (struct block_test_dev), GFP_KERNEL);
     bt_dev->queue = block_test_queue;
     bt_dev->queue->queuedata = bt_dev;
-    
+
+    bt_dev->size = DEV_SIZE;
+    strncpy(bt_dev->disk->disk_name, DEVICE_NAME, 32);
     bt_dev->disk->major = block_test_major;
     bt_dev->disk->first_minor = DEVICE_MINOR;
     bt_dev->disk->fops = &block_dev_fops;
-    //bt_set_capacity(bt_dev->disk, (bt_dev->size >> SECTOR_BITS));
     bt_dev->disk->queue = bt_dev->queue;
+    set_capacity(bt_dev->disk, (bt_dev->size >> SECTOR_BITS));
 
     add_disk(bt_dev->disk);
-
     return 0;
 
 out_queue:
@@ -119,10 +130,10 @@ static void __exit block_test_exit(void)
         blkdev_put(bt_dev);
     }
     */
-    
+
+    blk_cleanup_queue(bt_dev->queue);
     del_gendisk(bt_dev->disk);
     put_disk(bt_dev->disk);
-    blk_cleanup_queue(bt_dev->queue);
     unregister_blkdev(block_test_major, "block_test");
 }
 
